6d39f10ae71e97213102feb9f9723b5e8ef3b516
Gybernaty <118392182+TheMacroeconomicDao@users.noreply.github.com>
2023-03-29 21:53:33 +0300

    fix

--- GyberBackendContract.sol
+++ GyberBackendContract.sol
@@ -7,6 +7,9 @@ contract DataStorage {
     // Адрес владельца контракта
     address private owner;
 
+    // Адреса, которые могут вызывать функции контракта
+    address[] private authorizedAddresses;
+
     // Структура, описывающая данные
     struct Data {
         uint256 id;
@@ -34,8 +37,6 @@ contract DataStorage {
     modifier onlyAuthorized() {
         // Инициализация переменной-флага
         bool isAuthorized = false;
-        // Адреса, которые могут вызывать функции контракта
-        address[] memory authorizedAddresses = new address[](0);
         // Проверка, является ли адрес вызывающего авторизованным
         for (uint i = 0; i < authorizedAddresses.length; i++) {
             if (authorizedAddresses[i] == msg.sender) {
@@ -46,6 +47,21 @@ contract DataStorage {
         // В случае, если адрес не является авторизованным, генерируется исключение
         require(isAuthorized, "Not authorized");
         _;
+    }
+
+    // Функция добавления авторизованного адреса
+    function addAuthorizedAddress(address _address) public onlyOwner {
+        authorizedAddresses.push(_address);
+    }
+
+    // Функция удаления авторизованного адреса
+    function removeAuthorizedAddress(address _address) public onlyOwner {
+        for (uint i = 0; i < authorizedAddresses.length; i++) {
+            if (authorizedAddresses[i] == _address) {
+                delete authorizedAddresses[i];
+                break;
+            }
+        }
     }
 
     // Функция добавления данных в хранилище
@@ -66,14 +82,14 @@ contract DataStorage {
 
     // Функция обновления данных в хранилище
     function updateData(uint256 id, string memory dataType, address dataAddress, string memory link) public onlyAuthorized {
-        // Проверка, что данные с заданным ID есть в хранилище
-        require(data[id].id != 0, "Data with this ID does not exist");
-        // Обновление данных в хранилище
-        Data storage currentData = data[id];
-        currentData.dataType = dataType;
-        currentData.dataAddress = dataAddress;
-        currentData.link = link;
+    // Проверка, что данные с заданным ID есть в хранилище
+    require(data[id].id != 0, "Data with this ID does not exist");
+    // Обновление данных в хранилище
+    data[id].dataType = dataType;
+    data[id].dataAddress = dataAddress;
+    data[id].link = link;
     }
+
     // Функция удаления данных из хранилища
     function deleteData(uint256 id) public onlyAuthorized {
         // Проверка, что данные с заданным ID есть в хранилище
@@ -81,5 +97,4 @@ contract DataStorage {
         // Удаление данных из хранилища
         delete data[id];
     }
-
 }
