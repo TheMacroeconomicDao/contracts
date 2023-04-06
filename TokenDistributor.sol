a2254ef197ba352d40f456c98ec5186febb675c8
Arch <themacroeconomicdao@tutanota.com>
2023-04-06 05:14:51 +0300

    sec&perf TokenDist

--- TokenDistributor.sol
+++ TokenDistributor.sol
@@ -1,59 +1,59 @@
+
 // SPDX-License-Identifier: AGPL
 
 pragma solidity ^0.8.17;
 
 interface IBEP20 {
-    function transfer(address to, uint256 value) external returns (bool);
-    function balanceOf(address who) external view returns (uint256);
+function transfer(address to, uint256 value) external returns (bool);
+function balanceOf(address who) external view returns (uint256);
 }
 
-contract TokenDistributer {
-    address[] public recipientsList;
-    address public owner;
-    IBEP20 public token;
-    mapping(address => bool) public recipients;
+contract TokenDistributerGBR {
+address[] public recipientsList;
+uint256 public recipientsCount;
+address public owner;
+IBEP20 public token;
+mapping(address => bool) public recipients;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor(address _tokenAddress) {
-        owner = msg.sender;
-        token = IBEP20(_tokenAddress);
-    }
+constructor(address _tokenAddress) {
+    owner = msg.sender;
+    token = IBEP20(_tokenAddress);
+}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can perform this action");
-        _;
-    }
+modifier onlyOwner() {
+    require(msg.sender == owner, "Only the contract owner can perform this action");
+    _;
+}
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "New owner cannot be the zero address");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
+function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0), "New owner cannot be the zero address");
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+}
 
-    function addRecipient(address recipient) public onlyOwner {
+function addRecipient(address recipient) public onlyOwner {
+    if (!recipients[recipient]) {
         recipients[recipient] = true;
+        recipientsCount++;
     }
+}
 
-    function removeRecipient(address recipient) public onlyOwner {
+function removeRecipient(address recipient) public onlyOwner {
+    if (recipients[recipient]) {
         recipients[recipient] = false;
+        recipientsCount--;
     }
+}
 
-    function recipientsCount() public view returns (uint256 count) {
-        for (uint256 i = 0; i < recipientsList.length; i++) {
-            if (recipients[recipientsList[i]]) {
-                count++;
-            }
-        }
-    }
-
-    function distributeTokens() public onlyOwner {
-    require(token.balanceOf(address(this)) >= 1000000 * recipientsList.length, "Insufficient balance to distribute tokens");
+function distributeTokens() public onlyOwner {
+    require(token.balanceOf(address(this)) >= 1000000 * recipientsCount, "Insufficient balance to distribute tokens");
 
     for (uint256 i = 0; i < recipientsList.length; i++) {
-        if (recipientsList[i] != address(0)) {
+        if (recipients[recipientsList[i]]) {
             require(token.transfer(recipientsList[i], 1000000), "Token transfer failed");
-            }
         }
     }
+}
 }
